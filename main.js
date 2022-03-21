461e18face646012335261c7c04c2816c09b2ea3
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2022-03-21 18:14:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_hp="";
 song_pp="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload()
 {
@@ -13,6 +17,27 @@ function setup()
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist x= "+leftWristX+" left wrist y= "+leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist x= "+rightWristX+" right wrist y= "+rightWristY);
+    }
 }
 
 function draw()
