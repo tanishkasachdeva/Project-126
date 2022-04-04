7053c2567b03b35de477a5edf5aa7a759988bd74
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2022-04-04 23:23:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-song_hp="";
-song_pp="";
+song_hp1="";
+song_pp2="";
 leftWristX=0;
 leftWristY=0;
+leftWristScore=0;
 rightWristX=0;
 rightWristY=0;
+rightWristScore=0;
+songHP1_status="";
+songPP2_status="";
 
 function preload()
 {
-    song_hp=loadSound("harry_potter.mp3");
-    song_pp=loadSound("peter_pan.mp3");
+    song_hp1=loadSound("harry_potter.mp3");
+    song_pp2=loadSound("peter_pan.mp3");
 }
 
 function setup()
@@ -33,9 +37,11 @@ function gotPoses(results)
         console.log(results);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
+        leftWristScore=results[0].pose.keypoints[9].score;
         console.log("left wrist x= "+leftWristX+" left wrist y= "+leftWristY);
         rightWristX=results[0].pose.rightWrist.x;
         rightWristY=results[0].pose.rightWrist.y;
+        rightWristScore=results[0].pose.keypoints[9].score;
         console.log("right wrist x= "+rightWristX+" right wrist y= "+rightWristY);
     }
 }
@@ -43,10 +49,30 @@ function gotPoses(results)
 function draw()
 {
     image(video,0,0,600,500);
+
+    songHP1_status=song_hp1.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (leftWristScore>0.1)
+    {
+        circle(leftWristX,leftWristY,20);
+        song_pp2.stop();
+
+        if (songHP1_status==false)
+        {
+            song_hp1.play();
+            document.getElementById("song").innerHTML="Song: Harry Potter";
+        }
+    }
+
 }
 
-function end()
+function stop()
 {
-    song_hp.stop();
-    song_pp.stop();
+    song_hp1.stop();
+    song_pp2.stop();
+    document.getElementById("song").innerHTML="Song: ";
 }
+
+
